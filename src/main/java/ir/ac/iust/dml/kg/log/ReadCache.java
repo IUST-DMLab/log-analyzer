d351c77efa7bef4bb32e95b3ba43fb60bc948bc3
Ali Hadian <ali.hadian@gmail.com>
2017-05-05 15:25:11 +0430

    Add CacheReader and more stuff.

---
+++ src/main/java/ir/ac/iust/dml/kg/log/ReadCache.java
@@ -0,0 +1,19 @@
+package ir.ac.iust.dml.kg.log;
+
+import ir.ac.iust.dml.kg.resource.extractor.IResourceReader;
+import ir.ac.iust.dml.kg.resource.extractor.ResourceCache;
+import ir.ac.iust.dml.kg.resource.extractor.readers.ResourceReaderFromVirtuoso;
+
+/**
+ * Created by ali on 04/05/17.
+ */
+public class ReadCache {
+    public static void main(String[] args) throws Exception {
+        final ResourceCache cache = new ResourceCache("cache");
+        try (IResourceReader reader = new ResourceReaderFromVirtuoso("194.225.227.161", "1111",
+                "dba", "fkgVIRTUOSO2017", "http://localhost:8890/knowledgeGraphV2")) {
+
+            cache.cache(reader, 100000);
+        }
+    }
+}
