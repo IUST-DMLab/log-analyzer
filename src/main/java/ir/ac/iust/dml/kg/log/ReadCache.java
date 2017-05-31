cc52aabb80b9edb09388160df4493621e8ec1024
Ali Hadian <ali.hadian@gmail.com>
2017-05-31 15:10:03 +0100

    update resource-extractor version.

--- src/main/java/ir/ac/iust/dml/kg/log/ReadCache.java
+++ src/main/java/ir/ac/iust/dml/kg/log/ReadCache.java
@@ -12,7 +12,7 @@ public class ReadCache {
     public static void main(String[] args) throws Exception {
         final ResourceCache cache = new ResourceCache(args[0], true);
         try (IResourceReader reader = new ResourceReaderFromVirtuoso("194.225.227.161", "1111",
-                "dba", "fkgVIRTUOSO2017", "http://fkg.iust.ac.ir/")) {
+                "dba", "fkgVIRTUOSO2017", "http://fkg.iust.ac.ir/new")) {
 
             cache.cache(reader, 10000);
         }
